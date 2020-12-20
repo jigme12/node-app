c096289f7fdfdedbd14edcf9cd63268e4b8c122c
Jigme Sherpa <jigmedorjesherpa@outlook.com>
2020-12-20 17:07:54 +0545

    'event'

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const EventEmitter = require('events');
-const emitter = new EventEmitter();
+
+const EventEmitter= require('events');
+const emitter= new EventEmitter();
 //Register a listener
-emitter.on('messageLogged', function (arg){
-    console.log('Listener called', arg);
+emitter.on('messageLogged', function(){
+    console.log('Listener called');
 })
-//Raise an event 
-emitter.emit('messageLogged', {id: 1,url: 'hhtp://'});
+//Raise an event
+emitter.emit('messageLogged');
